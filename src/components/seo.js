60915c2a666d8749caed3a904836c003bdd034d4
Xavier <0xavier0@gmail.com>
2024-11-23 20:51:43 -0500

    fix: simplify Open Graph image URL assignment in SEO component

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,6 +28,7 @@ const Seo = ({ description, title, children }) => {
 
   const metaDescription = description || site.siteMetadata.description
   const defaultTitle = site.siteMetadata?.title
+  const imageUrl = `${site.siteMetadata.siteUrl}/images/Renees_Birthday_Party_Logo.jpg`
 
   return (
     <>
@@ -36,10 +37,7 @@ const Seo = ({ description, title, children }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:url" content={site.siteUrl} />
-      <meta
-        property="og:image"
-        content={site.siteUrl + "/images/Renees_Birthday_Party_Logo.jpg"}
-      />
+      <meta property="og:image" content={imageUrl} />
       <meta property="og:image:width" content="400" />
       <meta property="og:image:height" content="400" />
 
